abe51fd5aaa22f58841845536431409713eddc89
Johnson Chen <qiaosen.chen@gmail.com>
2023-04-16 18:19:18 +0800

    add my log toggle

---
+++ src/log/mylog.go
@@ -0,0 +1,38 @@
+package log
+
+/*
+ * Refer to https://blog.josejg.com/debugging-pretty/
+ * e.g. VERBOSE=0 go test -run 2A to disable logging
+ */
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+var verbose int
+
+func init() {
+	verbose = 1
+
+	verboseEnvStr := os.Getenv("VERBOSE")
+	if verboseEnvStr != "" {
+		var err error
+		verbose, err = strconv.Atoi(verboseEnvStr)
+		if err != nil {
+			log.Fatalf("invalid verbose value: %s", verboseEnvStr)
+		}
+	}
+
+	if verbose != 0 {
+		log.Printf("[init] verbose = %d", verbose)
+	}
+}
+
+func Debug(funcName string, format string, args ...interface{}) {
+	if verbose == 1 {
+		log.Printf(fmt.Sprintf("[%s] ", funcName)+format, args...)
+	}
+}
